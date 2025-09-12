f39ab13c573c9a9b9a2fcc544e0dc59758f61f51
Saradhi <bhumireddysaradhi9705@gmail.com>
2025-09-12 12:57:42 +0530

    added cors

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const __dirname = path.dirname(__filename);
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(cors("*"));
 
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
